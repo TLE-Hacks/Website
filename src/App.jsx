c3fbe5627319694f5c1bc03a2354094ccf6af803
Peterson Guo <petersonguo@gmail.com>
2022-12-01 09:09:57 -0500

    Init Template

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import "./App.css";
-import logo from "./logo.svg";
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Navbar } from "@/widgets/layout";
+import routes from "@/routes";
 
 function App() {
   return (
-    <div className="App" role="main">
-      <article className="App-article">
-        <img src={logo} className="App-logo" alt="logo" />
-        <h3>Welcome to React!</h3>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </article>
-    </div>
+    <>
+      <div className="container absolute left-2/4 z-10 mx-auto -translate-x-2/4 p-4">
+        <Navbar routes={routes} />
+      </div>
+      <Routes>
+        {routes.map(
+          ({ path, element }, key) =>
+            element && <Route key={key} exact path={path} element={element} />
+        )}
+        <Route path="*" element={<Navigate to="/home" replace />} />
+      </Routes>
+    </>
   );
 }
 
